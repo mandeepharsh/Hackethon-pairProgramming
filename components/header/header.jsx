b23b39bb98c5701966e3104fa701e7a9e60c2b94
Thomas Vovk <127060122+Thomasvovk@users.noreply.github.com>
2023-05-30 23:53:13 -0400

    version1

---
+++ components/header/header.jsx
@@ -0,0 +1,13 @@
+import "../header/header.scss";
+
+function Header() {
+  return (
+    <>
+      <div className="header">
+        <p className="header__logo">HA.TH</p>
+      </div>
+    </>
+  );
+}
+
+export default Header;
